8752a120950a15ac9b567409a07a2601afbc2b0f
Krishanathep <krishanathep@gmail.com>
2023-04-10 21:10:54 +0700

    install server package

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const mongoose = require("mongoose")
+require("dotenv").config()
+
+const app = express()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+//route
+app.get("*", (req,res)=>{
+    res.json({
+        data:"Message from server"
+    })
+})
+
+const port = process.env.PORT || 8080
+
+app.listen(port, ()=>console.log(`Start server in port ${port}`))
