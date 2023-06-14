efc54ce081e4908e969c957dd6ae7f2344e60d7c
krishanathep <39322839+krishanathep@users.noreply.github.com>
2023-06-14 12:42:56 +0700

    fix auth

--- routes/blogs.js
+++ routes/blogs.js
@@ -2,10 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { create, list, read, update, remove } = require("../controllers/blogsController");
 
-const { auth } = require("../Middleware/auth")
-
 router.post("/blogs", create);
-router.get("/blogs", auth, list);
+router.get("/blogs", list);
 router.get("/blogs/:id", read);
 router.put("/blogs/:id", update);
 router.delete("/blogs/:id", remove);
